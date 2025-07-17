94c21ec3b959f86a938740e37d0ec42e56d5e6fa
leticia-farias <leticiafarias.engsoftware@gmail.com>
2025-07-17 19:37:23 -0300

    feat: aplicar localStorage para salvar tema

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,13 +1,29 @@
 const toogleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement;
 
+function localTheme() {
+    const savedTheme = localStorage.getItem("data-theme");
+
+    if (savedTheme) {
+    document.documentElement.setAttribute('data-theme', savedTheme);
+  }
+}
+
 function changeTheme() {
     const currentTheme = rootHtml.getAttribute("data-theme");
 
-    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
+    if (currentTheme === "dark") {
+        rootHtml.setAttribute("data-theme", "light");
+        localStorage.setItem("data-theme", "light");
+    } else {
+        rootHtml.setAttribute("data-theme", "dark");
+        localStorage.setItem("data-theme", "dark");
+    }
+
     
     toogleTheme.classList.toggle("bi-sun")
     toogleTheme.classList.toggle("bi-moon-stars")
 }
 
+document.addEventListener("DOMContentLoaded", localTheme);
 toogleTheme.addEventListener("click", changeTheme);
